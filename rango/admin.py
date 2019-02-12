7608c434311765c980706dd720e57d6baabd701c
zuochujun <2359514z@student.gla.ac.uk>
2019-02-12 15:35:56 +0000

    9commit

--- rango/admin.py
+++ rango/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from rango.models import Category, Page
+from rango.models import UserProfile
 
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
@@ -7,3 +8,4 @@ class PageAdmin(admin.ModelAdmin):
 
 admin.site.register(Category)
 admin.site.register(Page, PageAdmin)
+admin.site.register(UserProfile)
