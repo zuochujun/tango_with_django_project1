380b242a285fd927be15655f1e468ac1f38691f2
zuochujun <2359514z@student.gla.ac.uk>
2019-02-12 07:11:48 +0000

    5 commit

--- rango/admin.py
+++ rango/admin.py
@@ -4,5 +4,6 @@ from rango.models import Category, Page
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
+
 admin.site.register(Category)
 admin.site.register(Page, PageAdmin)
--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -23,6 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
 url(r'^$', views.index, name='index'),
 url(r'^rango/', include('rango.urls')),
-
 url(r'^admin/', admin.site.urls),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
